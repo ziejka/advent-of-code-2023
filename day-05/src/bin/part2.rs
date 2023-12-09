9250ca53a0b91e10c4c65630b608c26f61d12026
ziejka <mm.zieja@gmail.com>
2023-12-09 13:33:24 +0100

    Remove unused code

--- day-05/src/bin/part2.rs
+++ day-05/src/bin/part2.rs
@@ -158,41 +158,6 @@ fn main() {
     let start = Instant::now();
 
     let _input = std::fs::read_to_string("src/bin/input").expect("file name input");
-    let _test = "seeds: 79 14 55 13
-
-seed-to-soil map:
-50 98 2
-52 50 48
-
-soil-to-fertilizer map:
-0 15 37
-37 52 2
-39 0 15
-
-fertilizer-to-water map:
-49 53 8
-0 11 42
-42 0 7
-57 7 4
-
-water-to-light map:
-88 18 7
-18 25 70
-
-light-to-temperature map:
-45 77 23
-81 45 19
-68 64 13
-
-temperature-to-humidity map:
-0 69 1
-1 0 69
-
-humidity-to-location map:
-60 56 37
-56 93 4"
-        .to_string();
-
     process(_input);
 
     let duration = start.elapsed();
