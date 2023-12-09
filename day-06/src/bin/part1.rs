55217ae37010573b2069e29b0d51d2fc3a13f33c
ziejka <mm.zieja@gmail.com>
2023-12-09 13:31:47 +0100

    Add day-06 project files

---
+++ day-06/src/bin/part1.rs
@@ -0,0 +1,10 @@
+fn process(s: &str) {
+    let lines = s.lines();
+}
+
+fn main() {
+    let _input = std::fs::read_to_string("src/bin/input").expect("file name input");
+    let _test = std::fs::read_to_string("src/bin/test").expect("file name input");
+
+    process(_test);
+}
