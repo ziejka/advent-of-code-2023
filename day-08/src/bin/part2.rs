114c8363b75d27ee327d1692b690c102c83f1758
ziejka <mm.zieja@gmail.com>
2023-12-10 20:46:57 +0100

    Solve part 2 of day 08 with Lowest Common Multiple

--- day-08/src/bin/part2.rs
+++ day-08/src/bin/part2.rs
@@ -1,24 +1,33 @@
-use std::collections::{HashMap, HashSet};
+use rayon::prelude::*;
+use std::collections::HashMap;
 
-fn step<'a>(
-    codes: &'a HashMap<String, [String; 2]>,
-    key: &String,
-    idx: &usize,
-    is_done: &mut bool,
-) -> &'a String {
-    *is_done = false;
+fn walk(
+    instructions_iter: &Vec<usize>,
+    codes: &HashMap<Box<str>, [Box<str>; 2]>,
+    start_key: &Box<str>,
+    num_steps: &mut usize,
+) -> u64 {
+    let mut key = start_key;
+    let has_won = instructions_iter.iter().any(|idx| {
+        *num_steps += 1;
+        let code = codes.get(key).unwrap();
+        key = code.get(*idx).unwrap();
 
-    let code = codes.get(key).unwrap();
-    let new_key = code.get(*idx).unwrap();
+        if key.ends_with('Z') {
+            return true;
+        }
+        return false;
+    });
 
-    if new_key.ends_with('Z') {
-        *is_done = true;
+    if has_won {
+        return *num_steps as u64;
     }
-    return new_key;
+
+    walk(instructions_iter, codes, key, num_steps)
 }
 
 fn process(s: String) {
-    let mut codes: HashMap<String, [String; 2]> = HashMap::new();
+    let mut codes: HashMap<Box<str>, [Box<str>; 2]> = HashMap::new();
 
     let s_without_parentheses = s.replace("(", "").replace(")", "");
     let mut lines = s_without_parentheses.lines();
@@ -36,69 +45,62 @@ fn process(s: String) {
 
     lines.skip(1).for_each(|line| {
         let mut parts = line.split("=");
-        let key = parts.next().unwrap().trim().to_string();
+        let key = parts.next().unwrap().trim().to_string().into_boxed_str();
 
         let values_iter = parts.next().unwrap().split(",").collect::<Vec<&str>>();
 
         codes.insert(
             key,
             [
-                values_iter[0].trim().to_string(),
-                values_iter[1].trim().to_string(),
+                values_iter[0].trim().to_string().into_boxed_str(),
+                values_iter[1].trim().to_string().into_boxed_str(),
             ],
         );
     });
 
-    let mut start_keys: HashSet<&String> = codes.keys().filter(|k| k.ends_with('A')).collect();
-    let mut num_steps: usize = 0;
+    let start_keys = codes
+        .keys()
+        .filter(|k| k.ends_with('A'))
+        .collect::<Vec<_>>();
 
-    walk(&mut start_keys, &codes, &mut num_steps, &instructions)
+    let steps = start_keys
+        .into_par_iter()
+        .map(|start_key| walk(&instructions, &codes, start_key, &mut 0))
+        .collect::<Vec<u64>>();
+
+    println!("min steps: {:?}", steps);
+    println!("result : {:?}", steps.lowest_common_multiple());
 }
 
-fn walk<'a>(
-    keys: &mut HashSet<&'a String>,
-    codes: &'a HashMap<String, [String; 2]>,
-    num_steps: &mut usize,
-    instructions: &'a Vec<usize>,
-) {
-    let mut all_done: bool = false;
-
-    while !all_done {
-        if *num_steps % 10000 == 0 {
-            println!("num_steps: {}", num_steps);
-        }
-        for idx in instructions.iter() {
-            *num_steps += 1;
-            all_done = true;
-
-            let mut temp_keys: Vec<&String> = Vec::new();
-
-            for start_key in keys.iter() {
-                let mut is_done = false;
-                let new_key = step(&codes, start_key, idx, &mut is_done);
-                temp_keys.push(new_key);
-                if !is_done {
-                    all_done = false;
-                }
-            }
-
-            keys.clear();
-            for new_key in temp_keys {
-                keys.insert(new_key);
-            }
-
-            if all_done {
-                println!("Found ZZZ in {} steps", num_steps);
-                return;
-            }
-        }
+fn greatest_common_divisor(a: u64, b: u64) -> u64 {
+    if b == 0 {
+        a
+    } else {
+        greatest_common_divisor(b, a % b)
     }
-    println!("Found ZZZ in {} steps", num_steps);
+}
+
+fn lowest_common_multiple(a: u64, b: u64) -> u64 {
+    a * b / greatest_common_divisor(a, b)
+}
+
+trait LowestCommonMultiple {
+    fn lowest_common_multiple(&self) -> u64;
+}
+
+impl LowestCommonMultiple for Vec<u64> {
+    fn lowest_common_multiple(&self) -> u64 {
+        self.into_iter()
+            .fold(1, |a, b| lowest_common_multiple(a, *b))
+    }
 }
 
 fn main() {
+    let start_time = std::time::Instant::now();
     let _input = std::fs::read_to_string("src/bin/input").expect("file name input");
-    let _test = std::fs::read_to_string("src/bin/test2").expect("file name input");
+    let _test = std::fs::read_to_string("src/bin/test").expect("file name input");
 
     process(_input);
+
+    println!("Finished in {:?}", start_time.elapsed());
 }
